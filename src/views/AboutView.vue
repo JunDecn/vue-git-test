1b6bfbef15f02b948f926ec52cefb6169396533a
a0963013523 <a0963013523@gmail.com>
2025-02-12 09:42:22 +0800

    test: revert github rule setting

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
-    <RouterLink to="/">Go to Home AAA</RouterLink>
+    <RouterLink to="/">Go to Home AAAA</RouterLink>
     <TheWelcome />
   </div>
 </template>
